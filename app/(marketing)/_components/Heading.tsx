065b0ba3b34116e5b4b8e950da38698fa2c035a3
MadNake <72208527+MadNake@users.noreply.github.com>
2023-12-27 20:28:53 +0100

    fix

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -15,7 +15,7 @@ export const Heading = () => {
 	return (
 		<div className="max-w-3xl space-y-4">
 			<h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-				Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Joiton</span>
+				Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Jotion</span>
 			</h1>
 			<h3 className="text-base sm:text-xl md:text-2xl font-medium">
 				Joiton is the connected workspace where <br />
@@ -37,7 +37,7 @@ export const Heading = () => {
 			{!isAuthenticated && !isLoading && (
 				<SignInButton mode="modal">
 					<Button>
-						get Joution free
+						get Jotion free
 						<ArrowRight className="h-4 w-4 ml-2"/>
 					</Button>
 				</SignInButton>
--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -45,7 +45,7 @@ export const Navbar = () => {
 					<>
 						<Button variant="ghost" size="sm" asChild>
 							<Link href="/documents">
-								Enter Joution
+								Enter Jotion
 							</Link>
 						</Button>
 						<UserButton
